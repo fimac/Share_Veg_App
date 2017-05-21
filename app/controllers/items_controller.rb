cfa9ec60f35615627583c47f956756c12a8a0d75
fimac <fimccawley@gmail.com>
2017-05-21 14:13:39 +1000

    item crud complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,38 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find_by(id: params['id'])
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.create ( item_params )
+    item.user = @current_user
+    item.save
+    redirect_to "items/#{item.id}"
   end
 
   def edit
+    @item = Item.find_by( id: params['id'] )
+  end
+
+  def update
+    item = Item.find_by( id: params['id'] )
+    item.update( item_params )
+    redirect_to "/items/#{item.id}"
+  end
+
+  def destroy
+    item = Item.find_by( id: params['id'] )
+    item.destroy
+    redirect_to "/items"
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user_id, :name, :description, :image)
   end
 end
