2601884ab3d13be1e48d98d5b9a16065994c2e27
katiepetersonn <katie.peterson19@gmail.com>
2017-05-21 14:24:17 +1000

    debugging seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,300 @@
 User.destroy_all
-
 u1 = User.create({
   name: "Vegodash",
   email: "vegodash@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
   image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
   bio: "I love the vegetables",
   likes: "veggies",
   dislikes: "chicken",
   allergies: "peanuts"
   })
-
 u2 = User.create({
   name: "Pine Apple",
   email: "pineapple@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
   image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
   bio: "I love the pineapples",
   likes: "pineapples",
   dislikes: "carrots",
   allergies: "watermelon"
   })
-
 u3 = User.create({
   name: "Mr Carrot",
   email: "carrot@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
   image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
   bio: "I love the carrots",
   likes: "carrots",
   dislikes: "pineapples",
   allergies: "cheese"
   })
-
+u4 = User.create({
+  name: "Dawton",
+  email: "ddawton0@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "yogurt",
+  allergies: "dairy"
+  })
+u5 = User.create({
+  name: "Studdard",
+  email: "studdard2@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the pineapples",
+  likes: "pineapples",
+  dislikes: "mayonnaise",
+  allergies: "eggs"
+  })
+u6 = User.create({
+  name: "Soffe",
+  email: "soffe3@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the carrots",
+  likes: "carrots",
+  dislikes: "tofu",
+  allergies: "soy"
+  })
+u7 = User.create({
+  name: "Haynes",
+  email: "haynes4@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "cereal",
+  allergies: "wheat"
+  })
+u8 = User.create({
+  name: "Lambshine",
+  email: "lambshine5@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the pineapples",
+  likes: "pineapples",
+  dislikes: "seafood",
+  allergies: "shellfish"
+  })
+u9 = User.create({
+  name: "Cornelissen",
+  email: "cornelissen6@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the carrots",
+  likes: "carrots",
+  dislikes: "Marzipan",
+  allergies: "cheese"
+  })
+u10 = User.create({
+  name: "Lippingwell",
+  email: "lippingwell7.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "liquorice",
+  allergies: "peanuts"
+  })
+u11 = User.create({
+  name: "Smead",
+  email: "msmead8@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "olives",
+  allergies: "corn"
+  })
+u12 = User.create({
+  name: "Charo",
+  email: "charo9@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the pineapples",
+  likes: "pineapples",
+  dislikes: "Brussels Sprouts",
+  allergies: "wheat"
+  })
+u13 = User.create({
+  name: "Hatje",
+  email: "ghatjea@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the carrots",
+  likes: "carrots",
+  dislikes: "Sour Cream",
+  allergies: "dairy"
+  })
+u14 = User.create({
+  name: "Scibsey",
+  email: "rscibseyb@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "Oysters",
+  allergies: "tree nuts"
+  })
+u15 = User.create({
+  name: "Haywood",
+  email: "jhaywoodc@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the pineapples",
+  likes: "pineapples",
+  dislikes: "Anchovies",
+  allergies: "soy"
+  })
+u16 = User.create({
+  name: "Harbertson",
+  email: "harbertsond@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the carrots",
+  likes: "carrots",
+  dislikes: "Sardines",
+  allergies: "fish"
+  })
+u17 = User.create({
+  name: "Denes",
+  email: "sdenese@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "raisens",
+  allergies: "peanuts"
+  })
+u18 = User.create({
+  name: "Bugbird",
+  email: "sbugbirdg@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the pineapples",
+  likes: "pineapples",
+  dislikes: "carrots",
+  allergies: "milk"
+  })
+u19 = User.create({
+  name: "Flecknoe",
+  email: "flecknoem@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the carrots",
+  likes: "carrots",
+  dislikes: "pineapples",
+  allergies: "eggs"
+  })
+u20 = User.create({
+  name: "Lerwill",
+  email: "slerwilln@email.com",
+  password: "chicken",
+  password_confirmation: "chicken",
+  image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+  bio: "I love the vegetables",
+  likes: "veggies",
+  dislikes: "chicken",
+  allergies: "soy"
+  })
   puts "User Count: #{User.all.count }"
-
   Item.destroy_all
-
   i1 = Item.create({
     name: "Pineapple",
     description: "I'm tasty and in good condition",
     image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
     user_id: u2.id
     })
-
   i2 = Item.create({
     name: "Carrot",
     description: "I'm in poor condition and not very tasty",
     image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
     user_id: u2.id
     })
-
   i3 = Item.create({
     name: "Apple",
     description: "I'm juicy and crunchy, yummmmmmy come and get me",
     image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
     user_id: u2.id
     })
-
+  i4 = Item.create({
+    name: "Brussel Sprouts",
+    description: "Whole bag of fresh Brussel Sprouts",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u4.id
+    })
+  i5 = Item.create({
+    name: "Olives",
+    description: "Home grown and made pickled olives",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u8.id
+    })
+  i6 = Item.create({
+    name: "Mushrooms",
+    description: "Straight out of my garden, heaps to share",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u8.id
+    })
+  i7 = Item.create({
+    name: "Organic Cheese",
+    description: "Cheese samples from a gift basket",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u11.id
+    })
+  i8 = Item.create({
+    name: "Carrot",
+    description: "I'm in poor condition and not very tasty",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u12.id
+    })
+  i9 = Item.create({
+    name: "Potatoes",
+    description: "Leftover uncooked potatoes from a 1kg bag",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u20.id
+    })
+  i10 = Item.create({
+    name: "Bread Rolls",
+    description: "Overstocked for a party and heaps of unused bread rolls",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u20.id
+    })
+  i11 = Item.create({
+    name: "Butter",
+    description: "Massive tub of butter",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u17.id
+    })
+  i12 = Item.create({
+    name: "Spinach",
+    description: "Bag of fresh spinach ideal for green juicing or salads",
+    image: "https://cdn.dribbble.com/users/107759/screenshots/3442108/galactic_1x.png",
+    user_id: u15.id
+    })
 puts "Item Count: #{Item.all.count }"
-
 Comment.destroy_all
-
 c1 = Comment.create ({
       post: "Your pineapple changed my life",
       user_id: u1.id,
@@ -69,5 +305,4 @@ c2 = Comment.create ({
       user_id: u3.id,
       item_id: i2.id
     })
-
 puts "Comment Count: #{Comment.all.count }"
