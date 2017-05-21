a6b69ab6430b5ec56704f2bc32511a44954c4d04
fimac <fimccawley@gmail.com>
2017-05-21 15:44:10 +1000

    comments crud added to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def show
+    @comment = Comment.find_by(id: params['id'])
   end
 
   def index
+    @all_comments = Comment.all
   end
 
   def new
+    
   end
 
   def edit
