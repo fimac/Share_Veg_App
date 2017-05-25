f26214308aa7a03dffcb0def47b43edf3f5f7a3e
katiepetersonn <katie.peterson19@gmail.com>
2017-05-25 16:23:50 +1000

    heroku fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 4 }
   validates :name, length: { minimum: 2 }
 
-  has_many :conversations, :foreign_key => :sender_id
+  has_many :conversations, :foreign_key => :sender_id, dependent: :destroy
   has_many :messages, through: :conversations, dependent: :destroy
 
   # UPVOTE
